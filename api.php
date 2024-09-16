bf467da26a6bcfcd032c5287ebc8fc0aedfff2db
kothatejaswini <kotha.tejaswini@globallogic.com>
2024-09-16 17:47:18 +0530

    Update api.php

--- api.php
+++ api.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // CORS headers
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
